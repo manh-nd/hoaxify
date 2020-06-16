7b3e1e4c7b1a218f93d79f8cab9fe68d5b5ff78a
manh-nd <manhnd.695@gmail.com>
2020-06-16 13:12:12 +0700

    init project

---
+++ hoaxify-service/src/main/java/com/iammanh/hoaxifyservice/HoaxifyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iammanh.hoaxifyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxifyServiceApplication.class, args);
+	}
+
+}
---
+++ hoaxify-service/src/test/java/com/iammanh/hoaxifyservice/HoaxifyServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iammanh.hoaxifyservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HoaxifyServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
